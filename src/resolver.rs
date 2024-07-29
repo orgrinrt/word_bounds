677f11b4dff395debc2ad35479293f9cf8794981
orgrinrt <ort@hiisi.digital>
2024-07-29 13:00:12 +0300

    remove the leftovers from earlier prototyping

--- src/resolver.rs
+++ src/resolver.rs
@@ -26,23 +26,7 @@ impl<'a, I: WordBoundResolverImpl<R>, R: ResolverRules> WordBoundResolver<'a, I,
 
 impl<'a, I: WordBoundResolverImpl<R>, R: ResolverRules> WordBoundResolver<'a, I, R> {
     pub fn resolve(s: &str) -> Vec<String> {
-        let mut raw = I::resolver(s);
-
-        // raw = raw.into_iter().filter(|word| {
-        //     if !R::special_chars_are_words() {
-        //         return contains_special_chars(word);
-        //     }
-        //     false
-        // }).collect();
-        //
-        // raw = raw.into_iter().map(|mut word| {
-        //     if R::skip_prepended_underscores() {
-        //         word = remove_prepended_underscores(word);
-        //     }
-        //     word
-        // }).collect();
-
-        raw
+        I::resolver(s)
     }
 
     #[inline]
@@ -54,24 +38,5 @@ impl<'a, I: WordBoundResolverImpl<R>, R: ResolverRules> WordBoundResolver<'a, I,
     pub fn run(mut self) -> Self {
         self.words = Self::resolve(self.input);
         self
-    }
-}
-
-#[inline]
-pub(crate) fn is_special_char(c: char) -> bool {
-    c == '-' || c == '_'
-}
-
-#[inline]
-pub(crate) fn contains_special_chars(word: &str) -> bool {
-    word.chars().any(|c| is_special_char(c))
-}
-
-#[inline]
-pub(crate) fn remove_prepended_underscores(word: String) -> String {
-    if word.starts_with('_') {
-        word.chars().skip_while(|&c| c == '_').collect()
-    } else {
-        word
     }
 }
